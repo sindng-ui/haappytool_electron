e2700b3f635f1fcfe5b5ada9c5b6243cc8c6bf6e
dongsim-ui <simjajeun@gmail.com>
2026-02-14 20:37:31 +0900

    remove obj/bin
    Signed-off-by: dongsim-ui <simjajeun@gmail.com>

--- App.tsx
+++ App.tsx
@@ -562,7 +562,7 @@ const AppContent: React.FC = () => {
         {isFocusMode && <div className="focus-ambient-glow animate-fade-in" />}
 
         <div className="flex-1 flex overflow-hidden z-10 relative">
-          <div className={`sidebar-transition ${isFocusMode ? 'sidebar-hidden w-0 opacity-0 -ml-64' : 'w-64 opacity-100'}`}>
+          <div className={`sidebar-transition ${isFocusMode ? 'sidebar-hidden w-0 opacity-0 -ml-14' : 'w-14 opacity-100'}`}>
             <Sidebar
               activePluginId={activeTool}
               onSelectPlugin={handleSetActiveTool}
